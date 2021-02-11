c4a8ff3ee8887543eddfdd80f4fc5780b5efc486
Pierre Penhard <p.penhard@gmail.com>
2021-02-11 22:30:39 +0100

    Added simple calendar and displayed booking on calendar

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,7 @@
+class BookingsController < ApplicationController
+  def index
+    # Scope your query to the dates being shown:
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @bookings = Booking.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :bookings
 
   has_many :student_relationships, foreign_key: :student_id, class_name: 'Booking'
-  has_many :students, through: :follower_relationships, source: :students
+  has_many :students, through: :student_relationships, source: :students
 
   has_many :teacher_relationships, foreign_key: :user_id, class_name: 'Booking'
   has_many :teacher, through: :teacher_relationships, source: :teachers
